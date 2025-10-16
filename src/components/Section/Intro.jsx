a93d8df488c91f63586e44490de87b74e1f86533
GiapThe2002 <giapthuy2002@gmail.com>
2025-10-16 14:31:20 +0700

    create intro section

--- src/components/Section/Intro.jsx
+++ src/components/Section/Intro.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { profileData } from "../../utils/data";
 
 const Intro = () => {
-  return <div>Intro</div>;
+  const { position, name, university, school, phone, email, portfolio, image } =
+    profileData;
+  return (
+    <header className="flex flex-col md:flex-row md:ml-6 items-center px-8 py-6 border-b border-gray-300 gap-4 font-times">
+      <div className="w-[120px] h-[120px] object-center rounded-[50%] overflow-hidden">
+        <img src={image} alt="Profile" className="scale-130" />
+      </div>
+      <div className="w-full">
+        <h2 className="text-2xl font-stretch-125% mb-1">{position}</h2>
+        <div className="flex flex-row justify-between items-center">
+          <div>
+            <h1 className="text-xl text-gray-700 font-medium">{name}</h1>
+            <p className="text-gray-700 text-md leading-none">{university}</p>
+            <p className="text-gray-700 text-md">{school}</p>
+          </div>
+          <div className="text-md text-right">
+            <p className="text-gray-800">{phone}</p>
+            <p className="text-gray-800 leading-none">{email}</p>
+            <a
+              href={portfolio}
+              className="text-gray-600 hover:underline font-medium"
+            >
+              Portfolio
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Intro;
---
+++ src/utils/data.js
@@ -0,0 +1,12 @@
+import profileImage from "../assets/images/profile.jpg";
+
+export const profileData = {
+  position: "Intern ReactJS",
+  name: "Thi-The GIAP",
+  university: "Hanoi University of Science & Technology",
+  school: "School of Electrical & Electronic Engineering",
+  phone: "+84-395318514",
+  email: "giapthe2002@gmail.com",
+  portfolio: "https://mochergt.id.vn/",
+  image: profileImage,
+};
